5bd10d395ffbdc7a9a5a314cd4121d4b522e70d5
Benjamin Vaysse <benjamin.vaysse@gmail.com>
2020-03-28 21:39:00 -0500

    add graph to main page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,123 @@
-import React from "react";
-//import styled from "styled-components";
-import BigContainer from "../containers/BigContainer";
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "@reach/router";
+import styled from "styled-components";
+import * as d3 from "d3";
+import {BorderRadius, Colors, Spacing, Typography} from "../rules";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+window.d3 = d3;
+const functionPlot = require("function-plot");
 
 const Home = () => {
+  const node = useRef(null);
+  const [functionText, setFunctionText] = useState("x^2");
+  useEffect(() => {
+    if (node.current) {
+      functionPlot({
+        target: node.current,
+        data: [
+          {
+            fn: functionText,
+          },
+        ],
+      });
+    }
+  }, [node, functionText]);
   return (
-    <BigContainer>
-      Hi there
-    </BigContainer>
+    <React.Fragment>
+      <Title>Peanut</Title>
+      <Subtitle>
+        Peanut is a website where you'll find several methods used to solve
+        numerical analysis problems.{" "}
+        <p>
+          <Link to={"/about"}>
+            <FontAwesomeIcon icon={["fas", "dot-circle"]} />
+            Learn more about this website
+          </Link>
+        </p>
+      </Subtitle>
+      <MainContainer>
+        <Graph ref={node} />
+        <Parameters>
+          <form>
+          <input
+            type="text"
+            name="functionText"
+            placeholder="x^2"
+            value={functionText}
+            onChange={e => setFunctionText(e.target.value)}
+          />
+          <button>Apply</button>
+          </form>
+        </Parameters>
+      </MainContainer>
+    </React.Fragment>
   );
 };
 
-//const Container = styled("div")``;
+const Title = styled("div")`
+  font-size: ${Typography.largeTitle.fontSize};
+  margin: ${Spacing.xl} ${Spacing.xxl} ${Spacing.xl};
+`;
+
+const Subtitle = styled("div")`
+  font-size: ${Typography.subTitle.fontSize};
+  margin: ${Spacing.xl} ${Spacing.xxl} ${Spacing.xl};
+  svg {
+    margin-right: ${Spacing.sm};
+  }
+`;
+
+const MainContainer = styled("div")`
+  margin: ${Spacing.xl} ${Spacing.xxl} ${Spacing.xl};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
+`;
+
+const Graph = styled("div")``;
+
+const Parameters = styled("div")`
+  display: flex;
+  flex-direction: column;
+  input {
+    width: 360px;
+    background: #fff;
+    color: ${Colors.utility.black.default};
+    font: inherit;
+    box-shadow: 0 6px 10px 0 rgba(0, 0, 0 , .1);
+    border: 0;
+    outline: 0;
+    padding: 22px 18px;
+  }
+  button {
+      display: inline-block;
+      color: inherit;
+      font: inherit;
+      border: 0;
+      outline: 0;
+      padding: 0;
+      transition: all 200ms ease-in;
+      cursor: pointer;
+      background: ${Colors.primary.ocean.default};
+      color: white;
+      box-shadow: 0 0 10px 2px rgba(0, 0, 0, .1);
+      border-radius: 2px;
+      padding: 12px 36px;
+      
+      &:hover {
+        background: darkblue;
+      }
+      
+      &:active {
+        box-shadow: inset 0 0 10px 2px rgba(0, 0, 0, .2);
+      }
+      margin-left: -85px;
+    }
+  }
+`;
 
 export default Home;
