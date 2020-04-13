eed85985d58001e7e42765bf632302f7c46eeae9
Benjamin Vaysse <benjamin.vaysse@gmail.com>
2020-04-13 15:40:47 -0500

    add try catch block for wrong function formula

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -3,46 +3,57 @@ import React, { useEffect, useRef, useState } from "react";
 import useWindowDimensions from "../utils/windowDimensionsHook";
 import {Colors, Spacing, Typography} from "../rules";
 
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 import styled from "styled-components";
 import * as d3 from "d3";
 window.d3 = d3;
 const functionPlot = require("function-plot");
 
-
 const Graph = () => {
   const size = useWindowDimensions();
   const node = useRef(null);
   const [functionText, setFunctionText] = useState("x^2");
+  const [errorMessage, setErrorMessage] = useState(null);
   const handleSubmit = event => {
     event.preventDefault();
+    setErrorMessage(null);
     setFunctionText(event.target.functionText.value);
   };
   useEffect(() => {
-    console.log(functionText);
     if (node.current) {
-      functionPlot({
-        target: node.current,
-        width: size.width > 800 ? 700 : size.width - 80,
-        xAxis: {
-          label: "x - axis",
-        },
-        yAxis: {
-          label: "y - axis",
-        },
-        height: 480,
-        data: [
-          {
-            fn: functionText,
+      try {
+        functionPlot({
+          target: node.current,
+          width: size.width > 800 ? 700 : size.width - 80,
+          xAxis: {
+            label: "x - axis",
           },
-        ],
-      });
+          yAxis: {
+            label: "y - axis",
+          },
+          height: 480,
+          data: [
+            {
+              fn: functionText,
+            },
+          ],
+        });
+      }
+      catch (err) {
+        setErrorMessage("The provided function isn't correct, please enter an other function")
+      }
+
     }
   }, [node, functionText, size.width]);
   return (
     <React.Fragment>
       <GraphTitle>f(x) = {functionText}</GraphTitle>
-      <GraphChart ref={node} />
+      {!errorMessage ? (<GraphChart ref={node} />) : (
+        <ErrorMessage>
+          <FontAwesomeIcon icon={"exclamation-circle"}/>
+          {errorMessage}
+        </ErrorMessage>)}
       <Parameters>
         <form onSubmit={handleSubmit}>
           <input type="text" name="functionText" placeholder="x^2" />
@@ -56,6 +67,14 @@ const Graph = () => {
 
 const GraphChart = styled("div")`
   margin: 0 0 ${Spacing.md} 0;
+`;
+
+const ErrorMessage = styled("div")`
+  text-align: center;
+  padding: ${Spacing.xl};
+  svg {
+    padding: 0 ${Spacing.md};
+  }
 `;
 
 const GraphTitle = styled("div")`
