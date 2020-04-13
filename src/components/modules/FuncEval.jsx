4b7370c08a18109c89a094cb60e9ed67d7a3daa4
Benjamin Vaysse <benjamin.vaysse@gmail.com>
2020-04-13 12:30:24 -0500

    template for method (in FuncEval.jsx)

--- src/components/modules/FuncEval.jsx
+++ src/components/modules/FuncEval.jsx
@@ -1,31 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 
-import {Spacing} from "../../rules";
-import {Title, Subtitle} from "../../containers/BigContainer";
+import {BorderRadius, Colors, Spacing} from "../../rules";
+import {Subtitle, Title} from "../../containers/BigContainer";
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 import styled from "styled-components";
 
 const FuncEval = () => {
+  const [pseudoCodeVisible, setPseudoCodeVisible] = useState(false);
   return (
     <React.Fragment>
       <Title>Function Evaluator</Title>
-      <Subtitle>
-        Enter a function, they chose the value (xi) that you want to evaluate.
-      </Subtitle>
+      <Subtitle>This method simply evaluates each value passed (x) with the function selected (f(x)).</Subtitle>
       <MainContainer>
+        <Side>
+          <SideTitle>Options</SideTitle>
+          <ToggleButton
+            active={!pseudoCodeVisible}
+            onClick={() => setPseudoCodeVisible(false)}
+          >
+            <FontAwesomeIcon icon={"wave-square"}/>
+            Functional example of the method
+          </ToggleButton>
+          <ToggleButton
+            active={pseudoCodeVisible}
+            onClick={() => setPseudoCodeVisible(true)}
+          >
+            <FontAwesomeIcon icon={"code"}/>
+            See the pseudocode
+          </ToggleButton>
+        </Side>
+        <Side grow>
+          <SideTitle>{pseudoCodeVisible ? "Pseudocode" : "Function"}</SideTitle>
+          {pseudoCodeVisible ? (
+            <PseudoCode>
+              There is no pseudocode for this function
+            </PseudoCode>
+          ) : (
+            <FuncEvalLive>
+              HEY
+            </FuncEvalLive>
+          )}
+        </Side>
       </MainContainer>
     </React.Fragment>
   );
 };
 
 const MainContainer = styled("div")`
-  margin: ${Spacing.xxl} ${Spacing.xl};
   display: flex;
+  margin: ${Spacing.xl} ${Spacing.xxl} ${Spacing.xl};
   flex-direction: row;
   @media (max-width: 1200px) {
     flex-direction: column;
   }
   align-items: left;
+`;
+
+const SideTitle = styled("h2")``;
+
+const ToggleButton = styled("button")`
+  display: block;
+  border: none;
+  font: inherit;
+  background-color: inherit;
+  color: ${props => props.active ? Colors.primary.ocean.lighter : Colors.utility.headline.default};
+  padding: ${Spacing.sm} 0;  
+  svg {
+    padding-right: ${Spacing.md};
+  }
+`;
+
+const Side = styled("div")`
+  margin-right: ${Spacing.xl};
+  flex-grow: ${props => props.grow && "1"};
+`;
+
+const PseudoCode = styled("code")`
+  background: ${Colors.utility.black.default};
+  color: ${Colors.utility.white.default};
+  border-radius: ${BorderRadius.md};
+  padding: 15px 20px 12px 22px;
+  display: block;
+  overflow-x: scroll;
+  width: 100%;
+  font-size: 16px;
+  line-height: 1.75;
+`;
+
+const FuncEvalLive = styled("div")`
+  background-color: white;
+  border-radius: ${BorderRadius.md};
+  flex-grow: 1;
+  width: 100%;
+  padding: ${Spacing.md} ${Spacing.lg};
 `;
 
 
