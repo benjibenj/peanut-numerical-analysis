b7573c83c2e875f6d744ada8e8a330483f5969c9
Alismos <daramirezs@eafit.edu.co>
2020-05-09 23:47:33 -0500

    multiple roots

--- src/components/methods/7multipleroots/multipleRootsFunction.js
+++ src/components/methods/7multipleroots/multipleRootsFunction.js
@@ -1,116 +1,78 @@
-import { format } from "mathjs";
+import {format, evaluate, abs, pow } from "mathjs";
 
-const multipleRootsFunction = (h, firstDerivate, secondDerivate, x0, tol = 10e-7, maxCount = 100) => {
+const multipleRootsFunction = (funct, firstDerivate, secondDerivate, x0, tol = 10e-7, maxCount = 100) => {
   let results = {
     iterations: [],
     conclusion: undefined,
   };
-  results.iterations.push([
-    0,
-    format(x0, { notation: "fixed", precision: 10 }),
-    format(0, { notation: "exponential", precision: 2 }),
-    undefined,
-  ]);
-  results.conclusion = "Method to do";
-  return results;
-  {/*
-  let error;
-  let count = 0;
-  let fX0 = evaluate(f, { x: x0 });
-  let fX1 = evaluate(f, { x: x1 });
-  let xN = x1 - (fX1 * (x1 - x0)) / (fX1 - fX0);
-  let fXN = evaluate(f, { x: xN });
+
   if (maxCount > 100) {
     maxCount = 100;
   }
+   
+  let fX = evaluate(funct, { x: x0 }); // we evaluate f(a)
+  let fXP = evaluate(firstDerivate, { x: x0 });
+  let fXS = evaluate(secondDerivate, { x: x0 });
+  let err = tol +1;
+  let d = pow(fXP, 2) - (fX*fXS);
+
+  let cont = 0;
+  
   results.iterations.push([
-    count,
+    cont,
     format(x0, { notation: "fixed", precision: 10 }),
-    format(fX0, { notation: "exponential", precision: 2 }),
-    undefined,
+    format(fX, { notation: "exponential", precision: 2 }),
+    ""
   ]);
-  if (fX0 === 0) {
+  
+  let  xEv;
+
+  do {
+        
+
+     xEv = x0 - (fX*fXP)/ (pow(fXP, 2) - (fX*fXS));
+     fX = evaluate(funct, { x: xEv });
+
+    fXP = evaluate(firstDerivate, { x: xEv });
+    fXS = evaluate(secondDerivate, { x: xEv });
+    err = abs(xEv - x0);
+    cont += 1;
+
+    
+    x0 = xEv;
+    results.iterations.push([
+      cont,
+      format(x0, { notation: "exponential", precision: 10 }),
+      format(fX, { notation: "exponential", precision: 2 }),
+      format(err, { notation: "exponential", precision: 2 }),
+    ]);
+
+    
+    
+  } while (err > tol && d !== 0 && cont < maxCount);
+
+  
+
+  if(fX === 0){
     results.conclusion =
-      "x" +
-      count +
-      " is the root : " +
+      "The root was found for x = " +
       format(x0, { notation: "fixed", precision: 15 });
     return results;
   }
-  count += 1;
-  results.iterations.push([
-    count,
-    format(x1, { notation: "fixed", precision: 10 }),
-    format(fX1, { notation: "exponential", precision: 2 }),
-    undefined,
-  ]);
-  if (fX1 === 0) {
+   else if (!(err <= tol)) {
     results.conclusion =
-      "x" +
-      count +
-      " is the root : " +
-      format(x1, { notation: "fixed", precision: 15 });
+      "An approximation of the root was found for x = " +
+      format(x0, { notation: "fixed", precision: 15 });
     return results;
-  }
-  count += 1;
-  error = abs(x1 - xN);
-  results.iterations.push([
-    count,
-    format(xN, { notation: "fixed", precision: 10 }),
-    format(fXN, { notation: "exponential", precision: 2 }),
-    format(error, { notation: "exponential", precision: 2 }),
-  ]);
-  if (fXN === 0) {
-    results.conclusion =
-      "x" +
-      count +
-      " is the root : " +
-      format(xN, { notation: "fixed", precision: 15 });
-    return results;
-  }
-  while (error > tol && count < maxCount && fXN !== 0) {
-    x0 = x1;
-    x1 = xN;
-    fX0 = evaluate(f, { x: x0 });
-    fX1 = evaluate(f, { x: x1 });
-    if (fX1 - fX0 !== 0) {
-      xN = x1 - (fX1 * (x1 - x0)) / (fX1 - fX0);
-      error = abs(x1 - xN); //error at the current step
-      count += 1;
-      results.iterations.push([
-        count,
-        format(xN, { notation: "fixed", precision: 10 }),
-        format(fXN, { notation: "exponential", precision: 2 }),
-        format(error, { notation: "exponential", precision: 2 }),
-      ]);
-    } else {
-      results.conclusion =
-        "The denominator is 0, the method cannot be continued. The last approximate root found is " +
-        format(xN, { notation: "fixed", precision: 15 }) +
-        " with an error of " +
-        format(error, { notation: "exponential", precision: 2 });
-      return results;
-    }
-  }
-  if (fXN === 0) {
-    results.conclusion =
-      "The root was found for xN = " +
-      format(xN, { notation: "fixed", precision: 15 });
-    return results;
-  } else if (error <= tol) {
-    results.conclusion =
-      "An approximation of the root was found for xN = " +
-      format(xN, { notation: "fixed", precision: 15 });
-    return results;
-  } else if (count === maxCount) {
+  } else if (cont === maxCount) {
     results.conclusion =
       "Given the number of iterations and the tolerance, it was impossible to find a satisfying root";
     return results;
   } else {
-    results.conclusion = "There was an unknown issue";
+    results.conclusion =
+      "There was an unknown issue";
     return results;
   }
-  */}
 };
 
 export default multipleRootsFunction;
