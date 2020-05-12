7be273126a04873f5b9544ca77270d451e0c5da9
Benjamin Vaysse <benjamin.vaysse@gmail.com>
2020-05-12 13:15:06 -0500

    add comments vandermonde function

--- src/components/methods/19Vandermonde/vandermondeFunction.js
+++ src/components/methods/19Vandermonde/vandermondeFunction.js
@@ -1,5 +1,3 @@
-import {randomInt} from "mathjs";
-
 const vandermondeFunction = (
   points
 ) => {
@@ -9,16 +7,16 @@ const vandermondeFunction = (
     ai: [[]],
     polynom: undefined
   };
-  let degree = points.x.length;
+  let degree = points.x.length; // number of points
   let matrixA = Array(degree);
   let B = Array(degree);
   let ai = Array(degree);
   for (let i = 0; i < degree; i++) {
     matrixA[i] = new Array(degree).fill(0);
-    B[i] = [points.y[i]];
-    ai[i] = ["a_"+(i+1)];
+    B[i] = [points.y[i]]; // B is a column matrix with yi values
+    ai[i] = ["a_"+(i+1)]; // Just for displaying the equation we're solving
     for (let j = 0; j < degree; j++) {
-      matrixA[i][j] = points.x[i]**(degree-j-1)
+      matrixA[i][j] = points.x[i]**(degree-j-1) // We create the Vandermonde matrix
     }
   }
   results.matrixA = matrixA;
@@ -26,6 +24,7 @@ const vandermondeFunction = (
   results.ai = ai;
   // Apply method to get x (gauss simple for example)
   results.polynom = [[0.412412027], [0.939374043], [5.836217905], [0.004699521], [183.1818]];
+  // To get these results, a method must be applied
   return results;
 };
 
