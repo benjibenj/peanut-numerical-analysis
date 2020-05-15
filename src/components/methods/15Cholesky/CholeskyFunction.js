76a108d98894e4d948dd93c168c8945cbabec34c
Alismos <daramirezs@eafit.edu.co>
2020-05-15 12:36:13 -0500

    Cholesky complete

--- src/components/methods/15Cholesky/CholeskyFunction.js
+++ src/components/methods/15Cholesky/CholeskyFunction.js
@@ -1,6 +1,6 @@
 import eye from "../../../utils/matrixFunctions/eye";
 import progressiveSustitution from "../../../utils/matrixFunctions/progressiveSustitution";
-import { usolve} from "mathjs";
+import { usolve, sqrt, complex, divide, multiply, add} from "mathjs";
 import deepCopyFunction from "../../../utils/deepCopyFunction";
 
 const choleskyFunction = (matrixA, B) => {
@@ -19,41 +19,59 @@ const choleskyFunction = (matrixA, B) => {
   let U = deepCopyFunction(eye(n));
  
 
-  for(let i = 0; i < n; i++){
+  for(let i = 0; i < n-1; i++){
 
-    for(let j=i; j < n; j++){
+    let productS1 = 0;
+      for(let k = 0; k < i; k++){
+        productS1 += L[i][k]*U[k][i];
+      }
+    L[i][i]=sqrt(matrixA[i][i]+(-productS1));
+    
+    U[i][i]=L[i][i];
+
+    for(let j = i+1; j < n; j++){
       let productS = 0;
       
       
       for(let k = 0; k < i; k++){
-        productS += L[j][k]*U[k][i];
+        productS = add(productS, L[j][k]*U[k][i]);
       }
-      L[j][i] = matrixA[j][i]-productS;
+      L[j][i] = divide(matrixA[j][i]+(-productS),U[i][i]);
+      
     }
 
     for (let j = i+1; j < n; j++){
       
       let productS = 0;
       for(let k = 0; k < i; k++){
-        productS += L[i][k]*U[k][j];
+        productS += add(productS, multiply(L[i][k],U[k][j]));
       }
-      
-      U[i][j] = (matrixA[i][j]+(-productS))/L[i][i];
+      U[i][j] = divide(matrixA[i][j]+(-productS),L[i][i]);
       
     } 
-    
     results.iterations.push({
       L: deepCopyFunction(L),
       U: deepCopyFunction(U),
     });
-  }
+    }
 
+  let productS = 0;
+  for(let k = 0; k < n-1; k++){
+    productS =add(productS, multiply(L[n-1][k],U[k][n-1]));
+  }
+  L[n-1][n-1] = sqrt(matrixA[n-1][n-1]-productS);
  
+  U[n-1][n-1]=L[n-1][n-1];
+
+  results.iterations.push({
+    L: deepCopyFunction(L),
+    U: deepCopyFunction(U),
+  });
   results.conclusion = "After applying regressive and progressive substitution we get :";
     let resultZ = progressiveSustitution(L,B);
     let resultX = usolve(U, resultZ);
 
-  results.finalSolution = resultX;
+  results.finalSolution = L;
   return results;
 };
 
