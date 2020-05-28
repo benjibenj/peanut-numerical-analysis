296d7d584af2ac814465e57575a57877494da621
Benjamin Vaysse <benjamin.vaysse@gmail.com>
2020-05-27 21:02:55 -0500

    add links to pseudocode & code for bisection

--- src/components/methods/2bisection/Bisection.jsx
+++ src/components/methods/2bisection/Bisection.jsx
@@ -57,6 +57,8 @@ const Bisection = ({ name }) => {
       title={name}
       prev={methods.find(method => method.index === 1)}
       next={methods.find(method => method.index === 3)}
+      jsAlgorithm={"https://github.com/benjamin-vaysse/peanut-numerical-analysis/blob/master/src/components/methods/2bisection/bisectionFunction.js"}
+      pseudoCode={"https://github.com/benjamin-vaysse/peanut-numerical-analysis/blob/master/src/components/methods/2bisection/pseudoCode/bisection.pdf"}
     >
       <LinkGraph>
         <Link to={"/graph?function=" + encodeURIComponent(functionText)}>
